e24969ce898eb868c9e6e32a2381ff650dca4064
Yegor <ekozinov280699@gmail.com>
2025-05-20 23:46:18 +0600

    add conftest

--- test/conftest.py
+++ test/conftest.py
@@ -1,18 +1,26 @@
 import asyncio
 import os
 
+from redis import asyncio as aioredis
+
 import pytest
+from fastapi_cache import FastAPICache
+from fastapi_cache.backends.redis import RedisBackend
 from httpx import AsyncClient, ASGITransport
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
 
 from core.models import Base, db_helper
 from main import main_app
+from test.inserts.all_models_insert import all_models_insert
+
+REDIS_HOST = os.getenv('APP_CONFIG__REDIS_DB__HOST', 'localhost')
+REDIS_PORT = os.getenv('APP_CONFIG__REDIS_DB__PORT', 6379)
 
 pytestmark = pytest.mark.asyncio(loop_scope="session")
 
 async_engine = create_async_engine(
-    url=os.getenv('APP_CONFIG__TEST_DB__TEST_DATABASE_URL'),
+    url=os.getenv('APP_CONFIG__TEST_DB__URL'),
     echo=False,
 )
 
@@ -28,7 +36,7 @@ async def async_db_engine():
         await conn.run_sync(Base.metadata.drop_all)
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 async def async_db(async_db_engine):
     async_session = async_sessionmaker(
         expire_on_commit=False,
@@ -39,9 +47,15 @@ async def async_db(async_db_engine):
     )
 
     async with async_session() as session:
-        await session.begin()
+        for table in reversed(Base.metadata.sorted_tables):
+            await session.execute(
+                text(f'TRUNCATE {table.name} RESTART IDENTITY CASCADE')
+            )
+
+        await all_models_insert(session)
 
         yield session
+        await session.commit()
 
         await session.rollback()
 
@@ -50,7 +64,7 @@ async def async_db(async_db_engine):
             await session.commit()
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='function')
 async def async_client(async_db) -> AsyncClient:
     async def override_get_db():
         try:
@@ -59,7 +73,23 @@ async def async_client(async_db) -> AsyncClient:
             await async_db.close()
 
     main_app.dependency_overrides[db_helper.session_getter] = override_get_db
-    return AsyncClient(transport=ASGITransport(app=main_app), base_url='http://localhost')
+    redis = aioredis.from_url(f"redis://{REDIS_HOST}:{REDIS_PORT}/0")
+    FastAPICache.init(RedisBackend(redis), prefix="test-cache")
+    yield AsyncClient(transport=ASGITransport(app=main_app), base_url='http://localhost')
+    await FastAPICache.clear()
+
+
+@pytest.fixture(scope='function')
+async def get_user_token(async_client):
+    async def _get_user_token(creds):
+        login_response = await async_client.post("/api/auth/jwt/login", data={
+            "username": f"{creds}@{creds}.com",
+            "password": f"{creds}"
+        })
+        token = login_response.json()["access_token"]
+        return token
+
+    return _get_user_token
 
 
 @pytest.fixture(scope='session')
