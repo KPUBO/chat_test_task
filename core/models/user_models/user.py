1a66c04cba084983b8c238376d3e7e052a9fea1f
Yegor <ekozinov280699@gmail.com>
2025-05-20 23:40:48 +0600

    add user model

--- core/models/user_models/user.py
+++ core/models/user_models/user.py
@@ -1,11 +1,28 @@
-from sqlalchemy import UniqueConstraint
-from sqlalchemy.orm import mapped_column, Mapped
+from datetime import datetime
+from typing import List
 
-from core.models import Base
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+from sqlalchemy import text, String
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-class User(Base):
-    username: Mapped[str] = mapped_column(unique=True)
-    foo: Mapped[int]
-    bar: Mapped[int]
+from core.models.base import Base
+from core.models.m2m_models.users_groups import users_groups
 
-    __table_args__ = (UniqueConstraint( 'foo', 'bar'),)
+
+class User(Base, SQLAlchemyBaseUserTable[int]):
+    name: Mapped[str] = mapped_column(String, unique=False)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"))
+    updated_at: Mapped[datetime] = mapped_column(
+        server_default=text("TIMEZONE('utc', now())"))
+
+    messages = relationship("Message", back_populates="sender")
+
+    groups: Mapped[List["Group"]] = relationship(
+        secondary=users_groups, back_populates="participants"
+    )
+
+    @classmethod
+    def get_db(cls, session: AsyncSession):
+        return SQLAlchemyUserDatabase(session, cls)
